276b7307987b093a31367f3f5fd72029c11deab7
Gilbert Hartmann <BertHartm@gmail.com>
2011-01-24 02:33:02 -0500

    allow users to pick the date to run for
    renamed clojure.jar to clojure-1.2.0.jar to be more specific

--- src/com/berthartm/android/Lectionary.clj
+++ src/com/berthartm/android/Lectionary.clj
@@ -4,19 +4,28 @@
    (:import (com.berthartm.android.Lectionary R$layout R$id))
    )
 
+(defn calcHolidays [year] (let [LC (new com.berthartm.android.LectionaryCalcs)]
+			    (def easter (.Easter LC year))
+			    (def pentecost (doto (.Easter LC year) (.add java.util.GregorianCalendar/DATE 49))) ; 50th day of Easter
+			    (def ashWednesday (doto (.Easter LC year) (.add java.util.GregorianCalendar/DATE -46))) ; 40 days + 6 Sundays before Easter
+			    (def transfiguration (doto (.Easter LC year) (.add java.util.GregorianCalendar/DATE -49))) ; Sunday before Ash Wednesday
+			    (def formatter (new java.text.SimpleDateFormat "MMM d"))
+			    (str "for " year
+				 "\nEaster is " (.format formatter (.getTime easter))
+				 "\nAsh Wednesday is " (.format formatter (.getTime ashWednesday))
+				 "\nTransfiguration is " (.format formatter (.getTime transfiguration))
+				 "\nPentecost is " (.format formatter (.getTime pentecost))
+				 "\nChrist the King is " (.format formatter (.getTime (.ChristTheKing LC year)))
+				 )))
+
+			   
+
 (defactivity Main
   (:create (.setContentView context R$layout/main)
-	   (let [tv (view-by-id R$id/holiday_info) GC (new java.util.GregorianCalendar) LC (new com.berthartm.android.LectionaryCalcs)]
-	     (def year (- (.get GC java.util.GregorianCalendar/YEAR) 1))
-	     (def easter (.Easter LC year))
-	     (def pentecost (doto (.Easter LC year) (.add java.util.GregorianCalendar/DATE 49))) ; 50th day of Easter
-	     (def ashWednesday (doto (.Easter LC year) (.add java.util.GregorianCalendar/DATE -46))) ; 40 days + 6 Sundays before Easter
-	     (def transfiguration (doto (.Easter LC year) (.add java.util.GregorianCalendar/DATE -49))) ; Sunday before Ash Wednesday
-	     (def formatter (new java.text.SimpleDateFormat "MMM d"))
-	     (.setText tv (str "for " year
-			       "\nEaster is " (.format formatter (.getTime easter))
-			       "\nAsh Wednesday is " (.format formatter (.getTime ashWednesday))
-			       "\nTransfiguration is " (.format formatter (.getTime transfiguration))
-			       "\nPentecost is " (.format formatter (.getTime pentecost))
-			       "\nChrist the King is " (.format formatter (.getTime (.ChristTheKing LC year)))
-			       )))))
+	   (let [tv (view-by-id R$id/holiday_info)]
+	     (.setText tv (calcHolidays (.get (new java.util.GregorianCalendar) java.util.GregorianCalendar/YEAR)))
+	     (on-click (view-by-id R$id/date_button)
+		       (let [dp (view-by-id R$id/main_date)]
+			 (.setText tv (calcHolidays (.getYear dp))))
+		       ))
+	   ))
