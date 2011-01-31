1c1a2902e74974f605db3b7a1f0417601beea3d9
Gilbert Hartmann <BertHartm@gmail.com>
2011-01-30 22:50:51 -0500

    fixed post New Year Non-Easter oriented dates. Now list all returned Sundays, not just the first.
    There might be an issue with the daily lectionary switching on Wednesdays with regard to the Easter / Christmas transitions (though not the liturgical year transitions)

--- src/com/berthartm/android/Lectionary.clj
+++ src/com/berthartm/android/Lectionary.clj
@@ -21,6 +21,7 @@
 
 ; we understand only Christmas and Easter Seasons, define everything else
 ; Christmas is in weeks, Easter in days
+; note that these definitions are unused at the moment
 (def Advent '(:Christmas -4))
 (def Epiphany '(:Christmas 2))
 (def AshWednesday '(:Easter -46))
@@ -35,50 +36,55 @@
 
 (def bigList '(
 	       ((:Date) 1 27 :A "Awesome Day")
+	       ((:Date) 1 1 :* "Name of Jesus")
 	       ((:Christmas -4) 1 0 :* "First Sunday of Advent")
 	       ((:Christmas 0) 1 0 :* "Christmas 1")
+	       ((:Christmas 0) 2 0 :* "Christmas 2")
 	       ((:Easter -49) 0 :A "Transfiguration")
 	       ))
 
 (defn applicableDay [liturgicalDay date]
-  ; todo: let year, xmas, xmas DoW, liturgicalYear
-  (cond (and (= (ffirst liturgicalDay) :Date)
-	     (= (nth liturgicalDay 1) (+ (.get date GregorianCalendar/MONTH) 1))
-	     (= (nth liturgicalDay 2) (.get date GregorianCalendar/DAY_OF_MONTH))
-	     (or (= (nth liturgicalDay 3) :*)
-		 (= (nth liturgicalDay 3) (.liturgicalYear (new com.berthartm.android.LectionaryCalcs) date))))
-	true
-	(and (= (ffirst liturgicalDay) :Easter)
-	     (or (= (nth liturgicalDay 2) :*)
-		 (= (nth liturgicalDay 2) (.liturgicalYear (new com.berthartm.android.LectionaryCalcs) date)))
-	     (.before (doto (.Easter (new com.berthartm.android.LectionaryCalcs) (.get date GregorianCalendar/YEAR)) (.add GregorianCalendar/DATE -50))
-		      date) ; transfiguration (day before)
-	     (.before date
-		      (doto (.Easter (new com.berthartm.android.LectionaryCalcs) (.get date GregorianCalendar/YEAR)) (.add GregorianCalendar/DATE 50))) ; pentecost (day after)
-	     (= (doto (.Easter (new com.berthartm.android.LectionaryCalcs) (.get date GregorianCalendar/YEAR)) (.add GregorianCalendar/DATE (+ (last (first liturgicalDay)) (nth liturgicalDay 1)))) date)
-	     )
-	true
-	(and (= (ffirst liturgicalDay) :Christmas)
-	     (or (= (nth liturgicalDay 3) :*)
-		 (= (nth liturgicalDay 3) (.liturgicalYear (new com.berthartm.android.LectionaryCalcs) date)))
-	     (or (.before date
-			  (doto (.Easter (new com.berthartm.android.LectionaryCalcs) (.get date GregorianCalendar/YEAR)) (.add GregorianCalendar/DATE -49))) ; transfiguration
-		 (.before (doto (.Easter (new com.berthartm.android.LectionaryCalcs) (.get date GregorianCalendar/YEAR)) (.add GregorianCalendar/DATE 49)) ; pentecost
-			  date))
-	     (= (doto (new GregorianCalendar (.get date GregorianCalendar/YEAR) 11 25)
-		  (.add GregorianCalendar/DATE (+ (* 7 (+ (last (first liturgicalDay)) ; Seasonal week offset
-							  (second liturgicalDay))) ; this day's week offset
-						  (cond (= (.get (new GregorianCalendar (.get date GregorianCalendar/YEAR) 11 25) java.util.GregorianCalendar/DAY_OF_WEEK) 1)
-							-7
-							true
-							(- 1 (.get (new GregorianCalendar (.get date GregorianCalendar/YEAR) 11 25) java.util.GregorianCalendar/DAY_OF_WEEK))
-							)
-						  (nth liturgicalDay 2))))
-		date)
-	     )
-	true
-	true false))
-					 
+  (let [year (.get date GregorianCalendar/YEAR)
+	litYear (.liturgicalYear (new com.berthartm.android.LectionaryCalcs) date)
+	xmas (cond (< (.get date GregorianCalendar/MONTH) 3)
+		   (new GregorianCalendar (- year 1) 11 25)
+		   true (new GregorianCalendar year 11 25))]
+    (cond (and (= (ffirst liturgicalDay) :Date)
+	       (= (nth liturgicalDay 1) (+ (.get date GregorianCalendar/MONTH) 1))
+	       (= (nth liturgicalDay 2) (.get date GregorianCalendar/DAY_OF_MONTH))
+	       (or (= (nth liturgicalDay 3) :*)
+		   (= (nth liturgicalDay 3) litYear)))
+	  true
+	  (and (= (ffirst liturgicalDay) :Easter)
+	       (or (= (nth liturgicalDay 2) :*)
+		   (= (nth liturgicalDay 2) litYear))
+	       (.before (doto (.Easter (new com.berthartm.android.LectionaryCalcs) year) (.add GregorianCalendar/DATE -50))
+			date) ; transfiguration (day before)
+	       (.before date
+			(doto (.Easter (new com.berthartm.android.LectionaryCalcs) year) (.add GregorianCalendar/DATE 50))) ; pentecost (day after)
+	       (= (doto (.Easter (new com.berthartm.android.LectionaryCalcs) year) (.add GregorianCalendar/DATE (+ (last (first liturgicalDay)) (nth liturgicalDay 1)))) date)
+	       )
+	  true
+	  (and (= (ffirst liturgicalDay) :Christmas)
+	       (or (= (nth liturgicalDay 3) :*)
+		   (= (nth liturgicalDay 3) litYear))
+	       (or (.before date
+			    (doto (.Easter (new com.berthartm.android.LectionaryCalcs) year) (.add GregorianCalendar/DATE -49))) ; transfiguration
+		   (.before (doto (.Easter (new com.berthartm.android.LectionaryCalcs) year) (.add GregorianCalendar/DATE 49)) ; pentecost
+			    date))
+	       (= (doto xmas ; should be safe to use xmas, as it's all in one doto clause
+		    (.add GregorianCalendar/DATE
+			  (+ (* 7 (+ (last (first liturgicalDay)) ; Seasonal week offset
+				     (second liturgicalDay))) ; this day's week offset
+			     (cond (= (.get xmas GregorianCalendar/DAY_OF_WEEK) 1) -7
+				   true (- 1 (.get xmas GregorianCalendar/DAY_OF_WEEK))
+				   ) ; find the Sunday (Prior, so 1st sunday of X can be a '1' in the declaration)
+			     (nth liturgicalDay 2)))) ; day offset (Sunday reading, Monday reading, etc.)
+		  date)
+	       )
+	  true
+	  true false))) ; base case, this day does not apply
+
 
 (defactivity Main
   (:create (.setContentView context R$layout/main)
@@ -87,7 +93,6 @@
 	     (on-click (view-by-id R$id/date_button)
 		       (let [dp (view-by-id R$id/main_date)]
 			 (.setText tv (calcHolidays (.getYear dp)))
-			 (.setText hol (last (first (filter (fn [possibleDay] (applicableDay possibleDay (new GregorianCalendar (.getYear dp) (.getMonth dp) (.getDayOfMonth dp)))) bigList))))
-			 )
-		       ))
+			 (.setText hol (apply str (interpose "\n" (map last (filter (fn [possibleDay] (applicableDay possibleDay (new GregorianCalendar (.getYear dp) (.getMonth dp) (.getDayOfMonth dp)))) bigList)))))
+			 )))
 	   ))
